d043072faa9eab8ee2ed3a998d195991dea73fde
LuongHuuLuan <19130131@st.hcmuaf.edu.vn>
2023-03-30 21:22:27 +0700

    config project

---
+++ src/routes/Route.js
@@ -0,0 +1,14 @@
+import Home from '~/Pages/Home';
+
+const employerRoutes = [
+    { path: '/', component: Home, layout: null },
+    // { path: "/job", component: null, layout: null },
+    // { path: "/profile", component: null, layout: null },
+    // { path: "/recruitment", component: null, layout: null },
+    // { path: "/login", component: null, layout: null },
+    // { path: "/register", component: null, layout: null },
+];
+
+const alumusRoutes = [];
+
+export { employerRoutes, alumusRoutes };
---
+++ src/routes/index.js
@@ -0,0 +1,2 @@
+
+export {alumusRoutes, employerRoutes} from './Route';
