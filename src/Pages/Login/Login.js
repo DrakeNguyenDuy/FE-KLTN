6366326dab11503bd33138081828b71f600f74e7
LuongHuuLuan <19130131@st.hcmuaf.edu.vn>
2023-04-01 23:44:47 +0700

    finish login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,49 +2,70 @@ import className from 'classnames/bind';
 import { Col, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { FcGoogle } from 'react-icons/fc';
 
 import styles from './Login.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 const cx = className.bind(styles);
 
 function Login() {
     return (
         <Row>
-            <Col lg={7} className={cx('login-content')}>
-                <h1>Đăng nhập ngay</h1>
-                <h3>Cơ hội việc làm tức thì</h3>
-                <p>
-                    Nêu bạn chưa có tài khoản <br />
-                    <span>
-                        Đăng ký{' '}
-                        <a
-                            href="https://figma.com/file/kdU1fpvg127UGy0b7QLXjy/Khóa-luận-tốt-nghiệp?node-id=0-1&t=kdHyQIdDkTGQBUMr-0"
-                            target="blank"
-                        >
-                            Tại đây
-                        </a>
-                    </span>
-                </p>
-                <img src="/assets/imgs/Saly-14.png" alt="Saly-14" />
+            <Col lg={7}>
+                <div className={cx('login-content', 'position-relative')}>
+                    <h1>Đăng nhập ngay</h1>
+                    <h2>Cơ hội việc làm ngay tức thì</h2>
+                    <p>
+                        Nêu bạn chưa có tài khoản <br />
+                        <span>
+                            Đăng ký{' '}
+                            <a
+                                href="https://figma.com/file/kdU1fpvg127UGy0b7QLXjy/Khóa-luận-tốt-nghiệp?node-id=0-1&t=kdHyQIdDkTGQBUMr-0"
+                                target="blank"
+                            >
+                                Tại đây!
+                            </a>
+                        </span>
+                    </p>
+                    <img src="/assets/imgs/Saly-14.png" alt="Saly-14" />
+                </div>
             </Col>
             <Col lg={5}>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                    </Form.Group>
+                <div className={cx('login-form')}>
+                    <h2>Đăng nhập</h2>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email </Form.Label>
+                            <Form.Control type="email" placeholder="Nhập email" />
+                            {/* <Form.Text className="text-danger">
+                                We'll never share your email with anyone else.
+                            </Form.Text> */}
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Mật khẩu</Form.Label>
+                            <Form.Control type="password" placeholder="Nhập mật khẩu" />
+                        </Form.Group>
+                        <Form.Group className={cx('login-fogotpass')}>
+                            <a href="forgotpass">Quên mật khẩu?</a>
+                        </Form.Group>
+                        <Button className={cx('login-button')} type="submit">
+                            Đăng nhập
+                        </Button>
+                        <Form.Group className={cx('login-with')}>
+                            <p>Hoặc đăng nhập với</p>
+                        </Form.Group>
+                        <Form.Group className={cx('login-social')}>
+                            <Button>
+                                <FontAwesomeIcon icon={faFacebook} style={{ color: '#6b0be0', fontSize: '40px' }} />
+                            </Button>
+                            <Button>
+                                <FcGoogle style={{ fontSize: '40px' }} />
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </div>
             </Col>
         </Row>
     );
