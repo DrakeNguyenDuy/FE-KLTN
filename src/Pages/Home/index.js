395f12917b78f7afe54cd93591316651a81053bd
LuongHuuLuan <luongluanmpt@gmail.com>
2023-04-17 20:58:08 +0700

    fix github page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,1 +1,4 @@
 export { default } from './Home';
+    // "homepage": "https://drakenguyenduy.github.io/FE-KLTN/",
+        // "predeploy": "npm run build",
+        // "deploy": "gh-pages -d build"
